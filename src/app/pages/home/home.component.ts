6fa9e7aed3f4382f45a0df9231e8e4b2350894e0
Kjeff24 <jeffreyarthur123@gmail.com>
2025-02-02 13:19:34 +0000

    Get all blog posts in home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { BlogPost } from '../../models/photo-blog';
-import { TokenResponse } from '../../models/token-response';
-import { PhotoBlogService } from '../../services/photo-blog-service/photo-blog.service';
 import { TokenService } from '../../services/token/token.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PhotoBlogService } from '../../services/photo-blog-service/photo-blog.service';
+import { TokenResponse } from '../../models/token-response';
+import { BlogPost } from '../../models/photo-blog';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
   code = '';
+  blogPost: BlogPost[] = [];
 
   constructor(
     private tokenService: TokenService,
     private activatedRoute: ActivatedRoute,
+    private photoBlogService: PhotoBlogService,
     private router: Router
   ) {}
 
@@ -28,6 +31,7 @@ export class HomeComponent {
       } else if (!this.tokenService.isLoggedIn()) {
         this.tokenService.login();
       } else {
+        this.getAllBlogPosts();
       }
     });
   }
@@ -44,5 +48,14 @@ export class HomeComponent {
     });
   }
 
-  
+  getAllBlogPosts(): void {
+    this.photoBlogService.getAllBlogPosts().subscribe({
+      next: (data: BlogPost[]) => {
+        this.blogPost = data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
