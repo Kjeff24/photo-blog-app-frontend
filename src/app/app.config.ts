358ffb14d557c1fbe8cd41f0b724a4af79781e86
Kjeff24 <jeffreyarthur123@gmail.com>
2025-02-08 15:20:15 +0000

    Fixed token interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { tokenInterceptor } from './interceptors/token.interceptor';
 
@@ -10,10 +9,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    {
-      provide: HTTP_INTERCEPTORS,
-      useValue: tokenInterceptor,
-      multi: true,
-    },
+    provideHttpClient( withInterceptors([tokenInterceptor])),
   ],
 };
