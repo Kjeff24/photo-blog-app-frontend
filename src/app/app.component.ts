70fd45cbbf377d9e8ff2739eafddbb391d425e4b
Kjeff24 <jeffreyarthur123@gmail.com>
2025-02-08 14:32:43 +0000

    Added dock menu to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DockMenuComponent } from './dock-menu/dock-menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, DockMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
