16a5ffcfd1da5901e3d37b173cf26fd65a0473b1
Kjeff24 <jeffreyarthur123@gmail.com>
2025-02-09 19:32:51 +0000

    Maintain position of bottom for dock menu

--- src/app/components/dock-menu/dock-menu.component.ts
+++ src/app/components/dock-menu/dock-menu.component.ts
@@ -1,5 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  Output,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MenuItem } from '../../models/photo-blog';
 import { Router } from '@angular/router';
@@ -8,35 +15,33 @@ import { Router } from '@angular/router';
   selector: 'app-dock-menu',
   imports: [CommonModule, FormsModule],
   templateUrl: './dock-menu.component.html',
-  styleUrl: './dock-menu.component.css'
+  styleUrl: './dock-menu.component.css',
 })
 export class DockMenuComponent {
-  position: string = 'right';
   activeUrl: string = '';
   menuItems: MenuItem[] = [
-      {
-        label: 'Home',
-        icon: 'assets/svg/home.svg',
-        url: '/',
-      },
-      {
-        label: 'My Photos',
-        icon: 'assets/svg/photos.svg',
-        url: '/photos',
-      },
-      {
-        label: 'Upload Photo',
-        icon: 'assets/svg/upload.svg',
-        url: '/upload',
-      },
-      {
-        label: 'Trash',
-        icon: 'assets/svg/trash.svg',
-        url: '/recycle-bin',
-      },
-    ];
+    {
+      label: 'Home',
+      icon: 'assets/svg/home.svg',
+      url: '/',
+    },
+    {
+      label: 'My Photos',
+      icon: 'assets/svg/photos.svg',
+      url: '/photos',
+    },
+    {
+      label: 'Upload Photo',
+      icon: 'assets/svg/upload.svg',
+      url: '/upload',
+    },
+    {
+      label: 'Trash',
+      icon: 'assets/svg/trash.svg',
+      url: '/recycle-bin',
+    },
+  ];
 
-  
   constructor(private el: ElementRef, private router: Router) {}
 
   onMenuItemClick(url: string): void {
