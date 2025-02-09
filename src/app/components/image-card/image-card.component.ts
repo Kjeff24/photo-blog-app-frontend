c30592b6c06fb436f65edd6aff316eb77b670445
Kjeff24 <jeffreyarthur123@gmail.com>
2025-02-09 21:04:05 +0000

    Use modal component in image card

--- src/app/components/image-card/image-card.component.ts
+++ src/app/components/image-card/image-card.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { BlogPost, MenuItem } from '../../models/photo-blog';
+import { BlogPost, CardMenuItem, MenuItem } from '../../models/photo-blog';
 import { ImagePreviewDockMenuComponent } from '../image-preview-dock-menu/image-preview-dock-menu.component';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-image-card',
-  imports: [CommonModule, ImagePreviewDockMenuComponent],
+  imports: [CommonModule, ImagePreviewDockMenuComponent, ModalComponent],
   templateUrl: './image-card.component.html',
   styleUrl: './image-card.component.css',
 })
@@ -13,10 +14,12 @@ export class ImageCardComponent {
   isPreviewOpen = false;
   previewIndex: string = '';
   rotate: number = 0;
-  @Input() menuItems: MenuItem[] = [];
+  isModalOpen = false;
+  selectedItem?: CardMenuItem;
+  @Input() menuItems: CardMenuItem[] = [];
   @Input() blogPost?: BlogPost;
   @Input() page: string = '';
-  @Output() menuItemClick = new EventEmitter<{ item: string, blogPost: BlogPost | null }>();
+  @Output() menuItemClick = new EventEmitter<{ label: string }>();
 
   openPreview(): void {
     if (this.blogPost?.photoId) {
@@ -40,5 +43,21 @@ export class ImageCardComponent {
 
   onClosePreview(): void {
     this.closePreview();
+  }
+
+  openModal(item: CardMenuItem): void {
+    this.selectedItem = item;
+    this.isModalOpen = true;
+  }
+
+  closeModal(): void {
+    this.isModalOpen = false;
+    this.selectedItem = undefined;
+  }
+
+  modalContinueResponse(label?: string): void {
+    if(this.blogPost && label) {
+      this.menuItemClick.emit({label: label})
+    }
   }
 }
