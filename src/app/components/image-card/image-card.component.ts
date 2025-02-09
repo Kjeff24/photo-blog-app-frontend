638f080d27368e0699bd33859b3c0830a5ba54f2
Kjeff24 <jeffreyarthur123@gmail.com>
2025-02-09 14:42:04 +0000

    Created image card component

---
+++ src/app/components/image-card/image-card.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { BlogPost } from '../../models/photo-blog';
+import { ImagePreviewDockMenuComponent } from '../image-preview-dock-menu/image-preview-dock-menu.component';
+
+@Component({
+  selector: 'app-image-card',
+  imports: [CommonModule, ImagePreviewDockMenuComponent],
+  templateUrl: './image-card.component.html',
+  styleUrl: './image-card.component.css',
+})
+export class ImageCardComponent {
+  isPreviewOpen = false;
+  previewIndex: string = '';
+  rotate: number = 0;
+  @Input() blogPost?: BlogPost;
+
+  openPreview(): void {
+    if (this.blogPost?.photoId) {
+      this.previewIndex = this.blogPost.photoId;
+      this.isPreviewOpen = true;
+    }
+  }
+
+  closePreview(): void {
+    this.isPreviewOpen = false;
+    this.resetImageSettings();
+  }
+
+  resetImageSettings(): void {
+    this.rotate = 0;
+  }
+
+  onRotateChange(newRotate: number): void {
+    this.rotate = newRotate;
+  }
+
+  onClosePreview(): void {
+    this.closePreview();
+  }
+}
