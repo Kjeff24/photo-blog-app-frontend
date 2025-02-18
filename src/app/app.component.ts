bd870561936dbc1cdf52bbc663110a05e51acc22
kjeff24 <jeffreyarthur123@gmail.com>
2025-02-18 14:11:05 +0000

    Added toastr to main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DockMenuComponent } from './components/dock-menu/dock-menu.component';
+import { ToastrComponent } from "./components/toastr/toastr.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, DockMenuComponent],
+  imports: [CommonModule, RouterOutlet, DockMenuComponent, ToastrComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
